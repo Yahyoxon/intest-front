78bd24980ab85f044fc1394065806b2dd10db141
Yahyo Temirov <yahyoxontemirov1997@gmail.com>
2022-07-04 05:22:23 +0500

    rqqw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,21 @@
 import Main from 'layouts/main';
-import type { GetServerSideProps, NextPage } from 'next';
+import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import { dehydrate, QueryClient, useQuery } from 'react-query';
 import { getAllData } from 'services/get/all';
 import { get } from 'lodash';
-import axios from 'axios';
 
 const HomeRoute: any = dynamic(() => import('routes/home'));
 
 const Home: NextPage = (props: any) => {
-  // const {data} = useQuery("products",() => getAllData("/products?_l=ru" ))
-  console.log(props);
+  const data = get(props, 'dehydratedState.queries[0].state.data');
+  console.log(data);
   return (
     <Main>
       <Head>
-        <title>
-          {get(props, 'dehydratedState.queries[0].state.data.data[0].name')}
-        </title>
+        <title>{get(data, 'data[1].name')}</title>
       </Head>
       <HomeRoute />
     </Main>
@@ -26,27 +23,15 @@ const Home: NextPage = (props: any) => {
 };
 export default Home;
 
-const fetchPokemons = async () => {
-  const { data } = await axios.get(
-    'http://api.in-test.uz/public/api/v1/products?_l=ru'
-  );
-  return JSON.parse(JSON.stringify(data));
-};
-
 export const getStaticProps: GetStaticProps = async (context) => {
   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery('products', fetchPokemons);
+  await queryClient.prefetchQuery('products', () =>
+    getAllData('/products?_l=ru')
+  );
   return {
     props: {
       dehydratedState: dehydrate(queryClient),
     },
   };
 };
-
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   return {
-//     paths: [],
-//     fallback: "blocking"
-//   };
-// };
--- src/services/get/all.tsx
+++ src/services/get/all.tsx
@@ -3,6 +3,6 @@ import axios from 'axios';
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getAllData = async (url: string) => {
-  const data = await axios.get(url);
+  const { data } = await axios.get(url);
   return JSON.parse(JSON.stringify(data));
 };
